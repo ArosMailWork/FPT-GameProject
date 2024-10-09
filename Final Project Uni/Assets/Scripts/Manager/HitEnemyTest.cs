1da433471c4d98c876e349ffc3ea5171093cc991
Duc Anh Le <rukomusic@gmail.com>
2024-10-09 09:20:27 +0700

    * fix material *Hit with OnTrigger

--- Final Project Uni/Assets/Scripts/Manager/HitEnemyTest.cs
+++ Final Project Uni/Assets/Scripts/Manager/HitEnemyTest.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class HitEnemyTest : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision objectWeHit)
+    public Vector3 offset;
+    public Rigidbody rb;
+
+    // Use OnTriggerEnter instead of OnCollisionEnter
+    private void OnTriggerEnter(Collider objectWeHit)
     {
-        if (objectWeHit.gameObject.CompareTag("Target"))
+        if (objectWeHit.CompareTag("Target"))
         {
-            Debug.Log("hit" + objectWeHit.gameObject.name + "!");
+            Debug.Log("hit " + objectWeHit.gameObject.name + "!");
             CreateParticle(objectWeHit);
             //Destroy(gameObject);
         }
     }
 
-    private void CreateParticle(Collision objectWeHit)
+    private void CreateParticle(Collider objectWeHit)
     {
-        ContactPoint contact = objectWeHit.contacts[0];
+        // Get the closest point of impact on the collider
+        Vector3 hitPoint = objectWeHit.ClosestPoint(transform.position);
 
+        // Calculate the normal direction from the collider's center to the hit point
+        Vector3 hitNormal = (hitPoint - objectWeHit.bounds.center).normalized;
+
+        // Adjust the rotation of the particle to align with the calculated normal
+        Quaternion rotation = Quaternion.LookRotation(hitNormal);
+
+        // Spawn particle at the hit point with the calculated normal
         GameObject particlePrefab = ParticleManager.Instance.SpawnOppositeParticle
-                (ParticleManager.Instance.prefab, contact.point, Quaternion.LookRotation(contact.normal));
+            (ParticleManager.Instance.prefab, hitPoint, rotation);
 
-        particlePrefab.transform.SetParent(objectWeHit.gameObject.transform);
+        // Offset the particle position slightly
+        particlePrefab.transform.position = hitPoint + offset;
     }
 }
